fbccb4d97c028b976707f75404b56fdad91a84c6
jess-ar <jessica18_24@hotmail.com>
2024-08-24 17:55:09 +0200

    [TB-41] feat:TB-42 Implement structure and style for the landing page in React. Adjustments to .gitignore and Tailwind variables

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import Button from "@/components/ui/Button";
-
+const homeImage = '/images/home.png';
 
 const Home = () => {
     const navigate = useNavigate();
 
     const handleExplore = () => {
-        navigate('/login'); // Cambia '/login' a la ruta correspondiente en tu aplicación
+        navigate('page1'); 
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-backgroundGray">
-            <h1 className="font-bold text-46 text-primaryBlue">Travel Buddy</h1>
-            <p className="mb-8 text-16 text-neutralGray">
-                Plan your next adventure with ease.
-            </p>
-            <Button className="mt-4" onClick={handleExplore}>
-                Start Exploring!
-            </Button>
+        <div 
+            className="relative flex flex-col items-center justify-center min-h-screen p-4 text-center bg-center bg-cover"
+            style={{ backgroundImage: `url(${homeImage})` }}>
+                <h1 className="text-5xl font-bold text-center font-roboto text-primaryBlue">Travel Buddy</h1>
+                <p className="mb-8 text-46 text-accentBlack">
+                    Plan your next adventure with ease.
+                </p>
+                <Button className="absolute w-full py-3 mt-4 font-semibold rounded-full text-16 text-secondaryWhite bg-primaryBlue bottom-[calc(5rem)]" onClick={handleExplore}>
+                    Start Exploring!
+                </Button>
         </div>
     );
 };
