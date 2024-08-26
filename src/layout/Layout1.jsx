0fef061e2bb18530cdf74af0c427bc71bab8b963
pilimuino <pili.muino@gmail.com>
2024-08-26 17:17:34 +0200

    [TB-36] feat: TB-38 Implement navbar navigation

--- src/layout/Layout1.jsx
+++ src/layout/Layout1.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 
 
-
 const Layout1 = () => {
     return (
         <>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,29 +1,52 @@
 import { createBrowserRouter } from 'react-router-dom';
+import App from '@/App'
 import Home from '@/pages/Home';
 import Page1 from '@/pages/Page1';
 import Page2 from '@/pages/Page2';
 import Page3 from '@/pages/Page3';
+import Layout1 from '@/layout/Layout1';
+import Layout2 from '@/layout/Layout2';
 import UnderConstruction from '@/components/UnderConstruction';
+
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />,
+        element: <App/>,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: 'page1',
+                element: <Page1/>,
+            },
+            {
+                path: 'page2', //Log in
+                element: <Page2 />,
+                errorElement: <UnderConstruction />
+            },
+            {
+                path: 'page3', // Create Account
+                element: <Page3 />,
+                errorElement: <UnderConstruction />
+            },
+            {
+                path: 'layout1',
+                element: <Layout1/>,
+            },
+            {
+                path: 'layout2',
+                element: <Layout2/>,
+            },
+            {
+                path: '*', // Welcome
+                element: <Home />,
+            },
+        ],
     },
-    {
-        path: '/page1', // Welcome
-        element: <Page1 />,
-    },
-    {
-        path: '/page2', //Log in
-        element: <Page2 />,
-        errorElement: <UnderConstruction />
-    },
-    {
-        path: '/page3', // Create Account
-        element: <Page3 />,
-        errorElement: <UnderConstruction />
-    },
+    
 ]);
 
 export default router;
